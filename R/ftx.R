127c113cc491fd2105e85dd97cf863dbfd51f19d
paulinenimo <paunimo@gmail.com>
2021-10-12 04:46:19 +0300

    Updating ftx_historical_prices function with logerror
    Adding checks for resolution, start time and end time arguments

--- R/ftx.R
+++ R/ftx.R
@@ -122,13 +122,28 @@ ftx_trades <- function(key, secret, market, start_time, end_time, ...) {
 ftx_historical_prices <- function(key, secret, market, resolution, start_time, end_time, ...) {
   # GET /markets/{market}/candles?resolution={resolution}&start_time={start_time}&end_time={end_time}
   # check if resolution, start_time and end_time are correct and not contradictory, log error if not
-  path = paste0('/api/markets/', market, '/candles?resolution=', resolution, '&start_time=', start_time, '&end_time=', end_time)
-  if(length(start_time) == 0){
-    path = paste0('/api/markets/', market, '/candles?resolution=', resolution, '&end_time=', end_time)
+  # check resolution parameter
+  res_values <- c(0, 15, 60, 300, 900, 3600, 14400, 86400)
+  if(resolution == 0 | !(resolution%%86400) %in% res_values) {
+    logerror(msg = 'Unacceptable resolution value. Acceptable values are 15, 60, 300, 900, 3600, 14400, 86400, or any multiple of 86400 up to 30*86400')
   }
-  if(length(end_time) == 0){
-    path = paste0('/api/markets/', market, '/candles?resolution=', resolution, '&start_time=', start_time)
+  
+  # check start and end times
+  if(!missing(start_time) & !missing(end_time)){
+    if(start_time > end_time){
+      logerror(msg = 'Start date cannot be after end date.')
+    }
   }
+  
+  path = paste0('/api/markets/', market, '/candles?resolution=', resolution)
+  
+  if(!missing(start_time)){
+    path = paste0(path, '?start_time=', start_time)
+  }
+  if(!missing(end_time)){
+    path = paste0(path, '?end_time=', end_time)
+  }
+  
   response = ftx_send_request(method = "GET", path = path, key, secret)
   result = response$result
   
