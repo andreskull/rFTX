6904840ac99a97d3d069d89a0f1e3d2fea251a59
paulinenimo <paunimo@gmail.com>
2021-11-01 21:26:56 +0300

    Updating ftx orders history

--- R/ftx.R
+++ R/ftx.R
@@ -453,7 +453,9 @@ ftx_orders_history <- function(key, secret, subaccount, markets=c(), ...) {
   df <- do.call(rbind, apply(tibble(r = result), 1, function(x) {
     df <- x[[1]] %>%
       replace(lengths(.) == 0, NA) %>% 
-      tibble::as_tibble()
+      tibble::as_tibble() %>%
+      mutate(createdAt = as.POSIXct(gsub("(.*):", "\\1", createdAt), 
+                                    format = "%Y-%m-%dT%H:%M:%OS%z"))
   }
   ))
   if(length(result) > 0 & !missing(markets)){
