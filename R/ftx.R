f228196dc0499e680def6f42f0b8ea2069d5c70d
paulinenimo <paunimo@gmail.com>
2021-11-01 21:29:05 +0300

    Updating ftx modify order

--- R/ftx.R
+++ R/ftx.R
@@ -559,7 +559,9 @@ ftx_modify_order <- function(key, secret, subaccount, order_id, size, price, ...
   
   df <- result %>%
     replace(lengths(.) == 0, NA) %>%
-    tibble::as_tibble()
+    tibble::as_tibble() %>%
+    mutate(createdAt = as.POSIXct(gsub("(.*):", "\\1", createdAt), 
+                                  format = "%Y-%m-%dT%H:%M:%OS%z"))
   
   return_obj <- list(
     success = response$success,
